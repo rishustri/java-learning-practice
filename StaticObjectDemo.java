61029b628eb950ea52ed12bf9424cb559647c941
Vaiibhavi_Singh <vaiibhavisingh855@gmail.com>
2026-08-31 22:31:28 +0530

    Staic method and instance method calling example

---
+++ StaticObjectDemo.java
@@ -0,0 +1,8 @@
+public class StaticObjectDemo{
+    static void show(){
+        System.out.println("Static method called");
+    }
+    public static void main(String[] args){
+        StaticObjectDemo.show();
+    }
+}

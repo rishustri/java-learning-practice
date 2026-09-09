f8f1b0793a619e5c2d655e7465aa2d4bd631d74d
Vaiibhavi_Singh <vaiibhavisingh855@gmail.com>
2026-09-09 11:10:58 +0530

    to find reactangle area using width and length , enetring  it in pricate in the main class and then using encapsulation that is getters and setter

--- Rectangle.java
+++ Rectangle.java
@@ -1,10 +1,38 @@
-class Rectangle{
-    int length, breadth;
-    Rectangle(int l, int b){
-        length=l;
-        breadth=b;
+class Rectangle {
+    private double length;  // private = not accessible outside
+    private double width;
+
+    // Setters - validate positive only
+    void setLength(double l) {
+        if (l > 0) {
+            this.length = l;
+        } else {
+            System.out.println("Length must be positive!");
+        }
     }
-    int area(){
-        return length*breadth;
+
+    void setWidth(double w) {
+        if (w > 0) {
+            this.width = w;
+        } else {
+            System.out.println("Width must be positive!");
+        }
+    }
+
+    // Getters
+    double getLength() { return length; }
+    double getWidth() { return width; }
+
+    void display() {
+        System.out.println("Length: " + length + ", Width: " + width);
+        System.out.println("Area: " + (length * width));
+    }
+
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle();
+        r.setLength(10);
+        r.setWidth(-5); // will print error
+        r.setWidth(5);
+        r.display();
     }
 }
